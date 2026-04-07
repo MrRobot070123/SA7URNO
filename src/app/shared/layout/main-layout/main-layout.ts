5513a22ad923cf860957f1dfb4724ac1b3ae8129
Daniel Pacheco <dapacheco33@hotmail.com>
2026-04-07 17:35:00 -0500

    Cotizaciones v1.1.1

--- src/app/shared/layout/main-layout/main-layout.ts
+++ src/app/shared/layout/main-layout/main-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
